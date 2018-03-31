bf32fdda4bcf046caa76abaa554d26553d0f96de
vidahaha <937634115@qq.com>
2018-03-31 19:35:48 +0800

    修改slider样式

--- src/component/Slider/Slider.js
+++ src/component/Slider/Slider.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
+import './Slider.less';
+
 
 class Slider extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            items:['我的任务', '个人信息', '团队信息'],
+        }
+    }
+
     render() {
         return(
-            <div className="Slider-warp">
-				111
+            <div className="slider-wrap">
+				{this.state.items.map((v, i) => {
+                    return <a className="item" key={i} href="task">{v}</a>;
+                })}             
 		    </div>
         );
     }
